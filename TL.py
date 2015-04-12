173e80f1b33c8d09988178d06907052346aadbdf
Maikel Wever <maikelwever@gmail.com>
2015-04-12 20:11:36 +0200

    Include API TL JSON and fix OOPness (imo)

--- TL.py
+++ TL.py
@@ -5,7 +5,9 @@ import json
 import io
 from numbers import Number
 
+
 class TlConstructor:
+
     def __init__(self, json_dict):
         self.id = int(json_dict['id'])
         self.type = json_dict['type']
@@ -26,7 +28,9 @@ class TlConstructor:
                 param['subtype'] = None
             self.params.append(param)
 
+
 class TlMethod:
+
     def __init__(self, json_dict):
         self.id = int(json_dict['id'])
         self.type = json_dict['type']
@@ -35,13 +39,16 @@ class TlMethod:
 
 
 class TLObject(dict):
+
     def __init__(self, tl_elem):
         self.name = tl_elem.predicate
 
+
 class TL:
+
     def __init__(self, filename):
         with open(filename, 'r') as f:
-           TL_dict = json.load(f)
+            TL_dict = json.load(f)
 
         # Read constructors
 
@@ -61,109 +68,129 @@ class TL:
             self.method_id[z.id] = z
             self.method_name[z.method] = z
 
+    def serialize_obj(self, type_, **kwargs):
+        bytes_io = io.BytesIO()
+        try:
+            tl_constructor = self.constructor_type[type_]
+        except KeyError:
+            raise Exception("Could not extract type: %s" % type_)
+        bytes_io.write(struct.pack('<i', tl_constructor.id))
+        for arg in tl_constructor.params:
+            serialize_param(bytes_io, type_=arg['type'],  value=kwargs[arg['name']])
+        return bytes_io.getvalue()
 
-## Loading TL_schema (should be placed in the same directory as mtproto.py
+    def serialize_method(self, type_, **kwargs):
+        bytes_io = io.BytesIO()
+        try:
+            tl_method = self.method_name[type_]
+        except KeyError:
+            raise Exception("Could not extract type: %s" % type_)
+        bytes_io.write(struct.pack('<i', tl_method.id))
+        for arg in tl_method.params:
+            serialize_param(bytes_io, type_=arg['type'], value=kwargs[arg['name']])
+        return bytes_io.getvalue()
+
+    def serialize_param(self, bytes_io, type_, value):
+        if type_ == "int":
+            assert isinstance(value, Number)
+            assert value.bit_length() <= 32
+            bytes_io.write(struct.pack('<i', value))
+        elif type_ == "long":
+            assert isinstance(value, Number)
+            bytes_io.write(struct.pack('<q', value))
+        elif type_ in ["int128", "int256"]:
+            assert isinstance(value, bytes)
+            bytes_io.write(value)
+        elif type_ == 'string' or 'bytes':
+            l = len(value)
+            if l < 254:  # short string format
+                bytes_io.write(struct.pack('<b', l))  # 1 byte of string
+                bytes_io.write(value)   # string
+                bytes_io.write(b'\x00'*((-l-1) % 4))  # padding bytes
+            else:
+                bytes_io.write(b'\xfe')  # byte 254
+                bytes_io.write(struct.pack('<i', l)[:3])  # 3 bytes of string
+                bytes_io.write(value)  # string
+                bytes_io.write(b'\x00'*(-l % 4))  # padding bytes
+
+    def deserialize(self, bytes_io, type_=None, subtype=None):
+        """
+        :type bytes_io: io.BytesIO object
+        """
+        assert isinstance(bytes_io, io.BytesIO)
+
+        # Built-in bare types
+        if type_ == 'int':
+            x = struct.unpack('<i', bytes_io.read(4))[0]
+        elif type_ == '#':
+            x = struct.unpack('<I', bytes_io.read(4))[0]
+        elif type_ == 'long':
+            x = struct.unpack('<q', bytes_io.read(8))[0]
+        elif type_ == 'double':
+            x = struct.unpack('<d', bytes_io.read(8))[0]
+        elif type_ == 'int128':
+            x = bytes_io.read(16)
+        elif type_ == 'int256':
+            x = bytes_io.read(32)
+        elif type_ == 'string' or type_ == 'bytes':
+            l = struct.unpack('<B', bytes_io.read(1))[0]
+            assert l <= 254  # In general, 0xFF byte is not allowed here
+            if l == 254:
+                # We have a long string
+                long_len = struct.unpack('<I', bytes_io.read(3)+b'\x00')[0]
+                x = bytes_io.read(long_len)
+                bytes_io.read(-long_len % 4)  # skip padding bytes
+            else:
+                # We have a short string
+                x = bytes_io.read(l)
+                bytes_io.read(-(l+1) % 4)  # skip padding bytes
+            assert isinstance(x, bytes)
+        elif type_ == 'vector':
+            assert subtype is not None
+            count = struct.unpack('<l', bytes_io.read(4))[0]
+            x = [deserialize(bytes_io, type_=subtype) for i in range(count)]
+        else:
+            # known types
+            try:
+                # Bare types
+                tl_elem = self.constructor_type[type_]
+            except KeyError:
+                # Boxed types
+                i = struct.unpack('<i', bytes_io.read(4))[0]  # read type ID
+                try:
+                    tl_elem = self.constructor_id[i]
+                except KeyError:
+                    # Unknown type
+                    raise Exception("Could not extract type: %s" % type_)
+
+            base_boxed_types = ["Vector t", "Int", "Long", "Double", "String", "Int128", "Int256"]
+            if tl_elem.type in base_boxed_types:
+                x = deserialize(bytes_io, type_=tl_elem.predicate, subtype=subtype)
+            else:  # other types
+                x = TLObject(tl_elem)
+                for arg in tl_elem.params:
+                    x[arg['name']] = deserialize(bytes_io, type_=arg['type'], subtype=arg['subtype'])
+        return x
+
+
+#  Loading TL_schema (should be placed in the same directory as mtproto.py
 tl = TL(os.path.join(os.path.dirname(__file__), "TL_schema.JSON"))
 
 
 def serialize_obj(type_, **kwargs):
-    bytes_io = io.BytesIO()
-    try:
-        tl_constructor = tl.constructor_type[type_]
-    except KeyError:
-        raise Exception("Could not extract type: %s" % type_)
-    bytes_io.write(struct.pack('<i', tl_constructor.id))
-    for arg in tl_constructor.params:
-        serialize_param(bytes_io, type_=arg['type'],  value=kwargs[arg['name']])
-    return bytes_io.getvalue()
+    return tl.serialize_obj(type_, **kwargs)
 
 
 def serialize_method(type_, **kwargs):
-    bytes_io = io.BytesIO()
-    try:
-        tl_method = tl.method_name[type_]
-    except KeyError:
-        raise Exception("Could not extract type: %s" % type_)
-    bytes_io.write(struct.pack('<i', tl_method.id))
-    for arg in tl_method.params:
-        serialize_param(bytes_io, type_=arg['type'], value=kwargs[arg['name']])
-    return bytes_io.getvalue()
+    return tl.serialize_method(type_, **kwargs)
 
 
 def serialize_param(bytes_io, type_, value):
-    if type_ == "int":
-        assert isinstance(value, Number)
-        assert value.bit_length() <= 32
-        bytes_io.write(struct.pack('<i', value))
-    elif type_ == "long":
-        assert isinstance(value, Number)
-        bytes_io.write(struct.pack('<q', value))
-    elif type_ in ["int128", "int256"]:
-        assert isinstance(value, bytes)
-        bytes_io.write(value)
-    elif type_ == 'string' or 'bytes':
-        l = len(value)
-        if l < 254: # short string format
-            bytes_io.write(struct.pack('<b', l))  # 1 byte of string
-            bytes_io.write(value)   # string
-            bytes_io.write(b'\x00'*((-l-1) % 4))  # padding bytes
-        else:
-            bytes_io.write(b'\xfe')  # byte 254
-            bytes_io.write(struct.pack('<i', l)[:3])  # 3 bytes of string
-            bytes_io.write(value) # string
-            bytes_io.write(b'\x00'*(-l % 4))  # padding bytes
+    return tl.serialize_param(bytes_io, type_, value)
+
 
 def deserialize(bytes_io, type_=None, subtype=None):
     """
     :type bytes_io: io.BytesIO object
     """
-    assert isinstance(bytes_io, io.BytesIO)
-
-    # Built-in bare types
-    if   type_ == 'int':    x = struct.unpack('<i', bytes_io.read(4))[0]
-    elif type_ == '#':      x = struct.unpack('<I', bytes_io.read(4))[0]
-    elif type_ == 'long':   x = struct.unpack('<q', bytes_io.read(8))[0]
-    elif type_ == 'double': x = struct.unpack('<d', bytes_io.read(8))[0]
-    elif type_ == 'int128': x = bytes_io.read(16)
-    elif type_ == 'int256': x = bytes_io.read(32)
-    elif type_ == 'string' or type_ == 'bytes':
-        l = struct.unpack('<B', bytes_io.read(1))[0]
-        assert l <= 254  # In general, 0xFF byte is not allowed here
-        if l == 254:
-            # We have a long string
-            long_len = struct.unpack('<I', bytes_io.read(3)+b'\x00')[0]
-            x = bytes_io.read(long_len)
-            bytes_io.read(-long_len % 4)  # skip padding bytes
-        else:
-            # We have a short string
-            x = bytes_io.read(l)
-            bytes_io.read(-(l+1) % 4)  # skip padding bytes
-        assert isinstance(x, bytes)
-    elif type_ == 'vector':
-        assert subtype is not None
-        count = struct.unpack('<l', bytes_io.read(4))[0]
-        x = [deserialize(bytes_io, type_=subtype) for i in range(count)]
-    else:
-        # known types
-        try:
-        # Bare types
-            tl_elem = tl.constructor_type[type_]
-        except KeyError:
-            # Boxed types
-            i = struct.unpack('<i', bytes_io.read(4))[0]  # read type ID
-            try:
-                tl_elem = tl.constructor_id[i]
-            except KeyError:
-                # Unknown type
-                raise Exception("Could not extract type: %s" % type_)
-
-        base_boxed_types = ["Vector t", "Int", "Long", "Double", "String", "Int128", "Int256"]
-        if tl_elem.type in base_boxed_types:
-            x = deserialize(bytes_io, type_=tl_elem.predicate, subtype=subtype)
-        else:  # other types
-            x = TLObject(tl_elem)
-            for arg in tl_elem.params:
-                x[arg['name']] = deserialize(bytes_io, type_=arg['type'], subtype=arg['subtype'])
-    return x
-
-
+    return tl.deserialize(bytes_io, type_=type_, subtype=subtype)
--- classes/telepy.py
+++ classes/telepy.py
@@ -1,22 +1,59 @@
+from TL import TL, deserialize
+
+import io
+import socket
+
 
 class Telepy():
-  def __init__(self):
-    # Deal with py2 and py3 differences
-    try: # this only works in py2.7
-      import configparser
-    except ImportError:
-      import ConfigParser as configparser
-    import mtproto
 
-    self._config = configparser.ConfigParser()
-    # Check if credentials is correctly loaded (when it doesn't read anything it returns [])
-    if not self._config.read('credentials'):
-      print("File 'credentials' seems to not exist.")
-      exit(-1)
-    ip = self._config.get('App data', 'ip_address')
-    port = self._config.getint('App data', 'port')
+    def __init__(self):
+        # Deal with py2 and py3 differences
+        try:  # this only works in py2.7
+            import configparser
+        except ImportError:
+            import ConfigParser as configparser
+        import mtproto
 
-    self._session = mtproto.Session(ip, port)
-    self._session.create_auth_key()
+        self._config = configparser.ConfigParser()
+        # Check if credentials is correctly loaded (when it doesn't read anything it returns [])
+        if not self._config.read('credentials'):
+            print("File 'credentials' seems to not exist.")
+            exit(-1)
+        ip = self._config.get('App data', 'ip_address')
+        port = self._config.getint('App data', 'port')
 
-    self._salt = future_salts = self._session.method_call('get_future_salts', num=3)
+        self._session = mtproto.Session(ip, port)
+        self._session.create_auth_key()
+
+        self._salt = future_salts = self._session.method_call('get_future_salts', num=3)
+
+        self.api_tl = TL('TL_telegram_api.JSON')
+
+    def call_api_method(self, method, **kwargs):
+        method_parameters = kwargs.copy()
+
+        if 'api_id' not in method_parameters:
+            method_parameters['api_id'] = self._config.getint('App data', 'api_id')
+
+        if 'api_hash' not in method_parameters:
+            method_parameters['api_hash'] = self._config.get('App data', 'api_hash')
+
+        for i in range(1, self._session.MAX_RETRY):
+            try:
+                self._session.send_message(self.api_tl.serialize_method(method, **method_parameters))
+                server_answer = self._session.recv_message()
+            except socket.timeout:
+                print("Retry API call method")
+                continue
+
+            try:
+                print(deserialize(io.BytesIO(server_answer)))
+            except Exception as e:
+                print(e)
+
+            try:
+                print(self.api_tl.deserialize(io.BytesIO(server_answer)))
+            except Exception as e:
+                print(e)
+
+            return
